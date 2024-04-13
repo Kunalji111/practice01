d53603ae6571cedaec0f7babe6f2081178eb57a9
kunal3227 <kunalanand3227@gmail.com>
2024-04-13 13:59:20 +0530

    connect to mongo atlas

--- src/app.js
+++
@@ -1,9 +0,0 @@
-function myFun() {
-    return(
-        <>
-        <h1>
-            kunal anand
-        </h1>
-        </>
-    )
-}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log("heelo")
+//require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+
+
+
+connectDB()
+
+
+/*
+import express from "express";
+const app = express()
+
+( async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) =>{
+            console.log("Eorr: ", error)
+        })
+
+        app.liseten(process.env.PORT, () => {
+            console.log(`App is listing on port ${process.env.PORT}`)
+        })
+    } catch {
+        console.log("ERROR: ", error)
+        throw error
+    }
+})()
+
+
+*/
