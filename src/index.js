1641343967597c59b6cb282466feecfd47dfc3a4
kunal3227 <kunalanand3227@gmail.com>
2024-04-15 11:55:41 +0530

    updating index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 //require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import express from "express";
 
 dotenv.config({
     path: './env'
 })
 
+const app = express();
 
 
+(async() =>{
+   await connectDB();
 
-connectDB()
+app.on("error",(error) => {
+    console.log("ERROR IS:",error)
+})
+app.listen(process.env.PORT,() => {
+    console.log(`\n App is listing on Port ${process.env.PORT}`)
+})
+})()
+
 
 
 /*
@@ -20,7 +31,7 @@ const app = express()
     try{
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         app.on("error", (error) =>{
-            console.log("Eorr: ", error)
+            console.log("Error: ", error)
         })
 
         app.liseten(process.env.PORT, () => {
